9b991ddb0bef7beedf87cfcde3eb46ba3e87b6c8
Kamran-Muradov <kamranshmu@code.edu.az>
2024-04-10 11:59:13 +0400

    I implemented created custom exceptions and constants

---
+++ Service/Helpers/Constants/ResponseMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace Service.Helpers.Constants
+{
+    public class ResponseMessages
+    {
+        public const string DataNotFound = "Data not found";
+        public const string InvalidIdFormat = "Id format is wrong";
+        public const string InvalidSortingFormat = "Sorting format is wrong";
+        public const string InvalidAgeFormat = "Age format is wrong";
+        public const string InvalidNameFormat = "Name format is wrong";
+        public const string InvalidSurnameFormat = "Surname format is wrong";
+        public const string AddSuccess = "Data added successfully";
+        public const string UpdateSuccess = "Data updated successfully";
+        public const string DeleteSuccess = "Data deleted successfully";
+    }
+}
